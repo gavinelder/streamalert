cbf77dfea1ff55c072ef0f5e1b7a52ce615c6848
Gavin Elder <gavin@improbable.io>
2022-10-13 12:53:11 +0100

    Move to pytest

--- tests/scripts/unit_tests.sh
+++ tests/scripts/unit_tests.sh
@@ -6,13 +6,15 @@ export AWS_SECRET_ACCESS_KEY=foobar_secret
 export AWS_SESSION_TOKEN=foobar_session_token
 export AWS_DEFAULT_REGION=us-east-1
 
-nosetests tests/unit \
---with-coverage \
---cover-erase \
---cover-package=streamalert \
---cover-package=streamalert_cli \
---cover-min-percentage=80 \
---cover-html \
---cover-html-dir=htmlcov \
---with-timer \
---timer-top-n=10
+pytest tests/unit --cov=src --cov-report=xml --cov-report=term-missing
+
+# nosetests tests/unit \
+# --with-coverage \
+# --cover-erase \
+# --cover-package=streamalert \
+# --cover-package=streamalert_cli \
+# --cover-min-percentage=80 \
+# --cover-html \
+# --cover-html-dir=htmlcov \
+# --with-timer \
+# --timer-top-n=10
